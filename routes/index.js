12ee9b4e35a072bc072daf81bb251b8c955f3314
mikle9997 <mikle9997@gmail.com>
2019-01-14 00:06:38 +0300

    next step is adding db

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,16 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/afisha', function(req, res, next) {
+  res.render('partition/afisha', { title: 'Express' });
+});
+
+router.get('/truppa', function(req, res, next) {
+  res.render('partition/truppa', { title: 'Express' });
+});
+
+router.get('/chulanchik', function(req, res, next) {
+  res.render('partition/chulanchik', { title: 'Express' });
+});
+
 module.exports = router;
