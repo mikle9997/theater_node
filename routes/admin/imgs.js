1028ad62dd11b5ba563ff7a4eebef8246b994c87
mikle9997 <mikle9997@gmail.com>
2019-01-25 14:10:16 +0300

    view of media

--- routes/admin/imgs.js
+++ routes/admin/imgs.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-
 router.get('/', async ( req, res, next ) => {
-    res.render('admin/imgs.ejs');
-});
+    const gallery = await db.media.getGallery();
+    const actors = await db.media.getActors();
+    const news = await db.media.getNews();
+    res.render('admin/imgs', { gallery, actors, news });
+  });
 
 module.exports = router;
